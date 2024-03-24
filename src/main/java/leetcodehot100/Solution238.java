5b927a6a7858a4a9ae039ba718bd05d11f20ed25
胥天昊 <1499487526@qq.com>
2024-03-24 16:33:25 +0800

    238

---
+++ src/main/java/leetcodehot100/Solution238.java
@@ -0,0 +1,39 @@
+package leetcodehot100;
+
+// [1,2,3,4]
+// left ：[1,1,2,6]
+// right: [24,12,4,1]
+// ans  : [24,12,8,6]
+
+/**
+ * 这道题要注意好边界的问题，和怎么对左数组赋值和如何给右数组赋值的就好了
+ */
+class Solution238 {
+    public int[] productExceptSelf(int[] nums) {
+        // 计算出数组长度
+        int n = nums.length;
+        // left用来存储除i以外的左边数组之积
+        int[] left = new int[n];
+        // right用来存储除i之外的右边数组之积
+        int[] right = new int[n];
+        // ans用来存储i的左右数组之积
+        int[] ans = new int[n];
+        // 初始化左数组
+        left[0] = 1;
+        // 初始化右数组
+        right[n - 1] = 1;
+        // 给左数组赋值
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+        // 给右数组赋值
+        for (int i = n - 2; i > -1; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+        // 结果集等于相应的左数组乘相应的右数组
+        for (int i = 0; i < n; i++) {
+            ans[i] = left[i] * right[i];
+        }
+        return ans;
+    }
+}
---
+++ src/main/java/leetcodehot100/Solution41.java
@@ -0,0 +1,28 @@
+package leetcodehot100;
+
+class Solution41 {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        // 对nums进行排序，排序成有顺序的
+        for (int i = 0; i < n; i++) {
+            while (nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        // 遍历就可以找出来了
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        // 如果到这里还没有返回，说明缺失的是最后一位
+        return n + 1;
+    }
+
+    // 交换函数
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
