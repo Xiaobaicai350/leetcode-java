6cca2c81d82b64db5a504cfd72495d7344b05da2
胥天昊 <1499487526@qq.com>
2024-03-24 17:44:29 +0800

    141

---
+++ src/main/java/leetcodehot100/Solution141.java
@@ -0,0 +1,28 @@
+package leetcodehot100;
+
+import haohao.ListNode;
+
+import java.util.HashSet;
+
+/**
+ * 快手真题，当时就做的这个
+ */
+public class Solution141 {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode cur = head;
+        while (cur != null) {
+            // 如果set中已经有这个内存地址了，说明又回来了
+            // 就说明有环了
+            if (set.contains(cur)) {
+                return true;
+            } else {
+                // 如果没有就说明这个不是，把这个添加到set中去
+                set.add(cur);
+            }
+            cur = cur.next;
+        }
+        // 如果到这里说明出循环了，就没环了
+        return false;
+    }
+}
