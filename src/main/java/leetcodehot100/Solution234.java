eff1f8aceb49852c87756cd9e7a0ee43ddc74728
胥天昊 <1499487526@qq.com>
2024-03-24 17:42:22 +0800

    234

---
+++ src/main/java/leetcodehot100/Solution234.java
@@ -0,0 +1,31 @@
+package leetcodehot100;
+
+import haohao.ListNode;
+
+import java.util.ArrayList;
+
+
+/**
+ * 这道题的方法就是把链表中的数据都存储到集合中，就好验证拉
+ * （注意，因为不知道这个链表有多大，所以用集合，因为集合可以动态扩缩容）
+ */
+class Solution234 {
+    public boolean isPalindrome(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ListNode cur = head;
+        // 把元素都加入到集合中，方便验证回文
+        while (cur != null) {
+            list.add(cur.val);
+            cur = cur.next;
+        }
+        //直接用i和j指向数组的最左边和最右边，然后开始验证
+        for (int i = 0, j = list.size() - 1; i < j; i++, j--) {
+            if (!list.get(i).equals(list.get(j))) {
+                // 如果不相等，说明不回文,直接返回false
+                return false;
+            }
+        }
+        // 到这里说明都相等，说明是回文子串
+        return true;
+    }
+}
