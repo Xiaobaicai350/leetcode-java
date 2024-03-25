a765c69e21e29177d18f314d657e708f6c2d11c4
胥天昊 <1499487526@qq.com>
2024-03-25 13:26:51 +0800

    94

---
+++ src/main/java/leetcodehot100/Solution94.java
@@ -0,0 +1,26 @@
+package leetcodehot100;
+
+import haohao.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 二叉树的中序遍历
+ */
+class Solution94 {
+    ArrayList<Integer> res = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inorder(root);
+        return res;
+    }
+
+    void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        res.add(root.val);
+        inorder(root.right);
+    }
+}
