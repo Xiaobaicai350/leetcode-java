ef46b9b5994229a04c0f87f1e78c2f57cdaff6b1
胥天昊 <1499487526@qq.com>
2024-03-25 13:57:45 +0800

    226

---
+++ src/main/java/leetcodehot100/Solution226.java
@@ -0,0 +1,21 @@
+package leetcodehot100;
+
+import haohao.TreeNode;
+
+
+class Solution226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        //进行翻转根节点
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        //递归的翻转左右子树
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
