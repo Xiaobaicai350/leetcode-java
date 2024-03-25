e84a92a4f6906ff7628fe05ac5c44b1814368240
胥天昊 <1499487526@qq.com>
2024-03-25 19:20:13 +0800

    543

---
+++ src/main/java/leetcodehot100/Solution543.java
@@ -0,0 +1,37 @@
+package leetcodehot100;
+
+import haohao.TreeNode;
+
+
+/**
+ * 二叉树的 直径 是指树中任意两个节点之间最长路径的 长度 。这条路径 可能经过 也可能 不经过根节点 root 。
+ * 两节点之间路径的 长度 由它们之间边数(注意这里)表示。
+ */
+class Solution543 {
+    //用来存储结果
+    int ans;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    /**
+     * 这个函数的意思就是以node为节点的子树的最长深度
+     *
+     * @param node
+     * @return
+     */
+    public int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        //分别记录左右支的最长长度
+        int lLen = dfs(node.left);
+        int rLen = dfs(node.right);
+        //给结果赋值
+        ans = Math.max(ans, lLen + rLen);
+        return Math.max(lLen, rLen) + 1;// 返回该节点为根的子树的深度
+    }
+
+}
