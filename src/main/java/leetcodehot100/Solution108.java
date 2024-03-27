7eb40f1341e966b1da16d09eae8c92eac9bb2a71
胥天昊 <1499487526@qq.com>
2024-03-27 17:02:44 +0800

    108

---
+++ src/main/java/leetcodehot100/Solution108.java
@@ -0,0 +1,35 @@
+package leetcodehot100;
+
+
+import haohao.TreeNode;
+
+import java.util.Arrays;
+
+
+class Solution108 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        //安全性校验
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildBST(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        //选择中间的作为根节点
+        int mid = (start + end) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        //传入左半部分
+        root.left = buildBST(nums, start, mid - 1);
+        //传入右半部分
+        root.right = buildBST(nums, mid + 1, end);
+
+        return root;
+    }
+
+}
