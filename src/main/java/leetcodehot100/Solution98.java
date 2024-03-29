7c0cad639c89806598125457cc1d6bca3ece2ddc
胥天昊 <1499487526@qq.com>
2024-03-29 11:08:47 +0800

    98

---
+++ src/main/java/leetcodehot100/Solution98.java
@@ -0,0 +1,42 @@
+package leetcodehot100;
+
+import haohao.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * 这道题的思路就是先给二叉树转换成数组，然后验证这个数组是否有序就可以了，因为二叉搜索树的中序遍历必然是有序的
+ */
+class Solution98 {
+    List<Integer> list = new ArrayList<>();
+
+    public boolean isValidBST(TreeNode root) {
+        //中序遍历 给list数组赋值
+        inorder(root);
+        //验证数组是否有序
+        for (int i = 1; i < list.size(); i++) {
+            //将后一个与前一个进行相比。注意这里等于也不行，具体看题目中的二叉树的定义
+            if (list.get(i) <= list.get(i - 1)) {
+                return false;
+            }
+        }
+        //如果之前的数组有序，也就是升序，返回true
+        return true;
+    }
+
+    /**
+     * 中序遍历，目的是给list数组赋值
+     *
+     * @param node
+     */
+    public void inorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left);
+        list.add(node.val);
+        inorder(node.right);
+    }
+}
