7e988d1d622486ad0c8b30203d98a6b514da5e65
胥天昊 <1499487526@qq.com>
2024-03-29 11:29:36 +0800

    199

---
+++ src/main/java/leetcodehot100/Solution199.java
@@ -0,0 +1,40 @@
+package leetcodehot100;
+
+import haohao.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+/**
+ * 这道题其实是借助一个队列来实现逐层遍历，只是在某一层的最后一个位置把最后一个节点取出来了而已。
+ * 逐层遍历也有对应的题
+ */
+class Solution199 {
+    public List<Integer> rightSideView(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        queue.addFirst(root);
+        for (; !queue.isEmpty(); ) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.removeLast();
+                if (node.left != null) {
+                    queue.addFirst(node.left);
+                }
+                if (node.right != null) {
+                    queue.addFirst(node.right);
+                }
+                //如果遍历到最后一个节点，就把他添加到结果集中
+                if (i == size - 1) {
+                    res.add(node.val);
+                }
+            }
+        }
+        return res;
+    }
+}
