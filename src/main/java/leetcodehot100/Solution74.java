47173e5ba9396c51255df377db969c445a330001
胥天昊 <1499487526@qq.com>
2024-04-07 14:53:58 +0800

    74

---
+++ src/main/java/leetcodehot100/Solution74.java
@@ -0,0 +1,29 @@
+package leetcodehot100;
+
+class Solution74 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // 进行安全性校验
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        // 获取行高和列高
+        int ls = matrix.length;
+        int rs = matrix[0].length;
+        // 初始化遍历二维数组的位置为左下角，这个位置是固定的，因为这个二维数组是有序的，可以进行简化
+        int l = ls - 1;
+        int r = 0;
+        while (l >= 0 && r < rs) {
+            if (matrix[l][r] > target) {
+                // 如果当前值比目标值大，说明要往上找
+                l--;
+            } else if (matrix[l][r] < target) {
+                // 如果当前值比目标值小，说明要往右边找
+                r++;
+            } else { // 说明当前值==目标值了，这时候就可以返回true了
+                return true;
+            }
+        }
+        // 如果到这里都找不到，说明这个二维数组里面没有target
+        return false;
+    }
+}
