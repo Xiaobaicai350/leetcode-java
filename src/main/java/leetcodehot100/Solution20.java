2e33ed137d05b87216d81a6a80429ed3908aa1cd
胥天昊 <1499487526@qq.com>
2024-04-09 09:50:41 +0800

    20

---
+++ src/main/java/leetcodehot100/Solution20.java
@@ -0,0 +1,36 @@
+package leetcodehot100;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+class Solution20 {
+    public boolean isValid(String s) {
+        int n = s.length();
+        //如果长度为奇数，直接返回
+        if (n % 2 == 1) {
+            return false;
+        }
+        //注意这个map里面的数据是反着的
+        Map<Character, Character> pairs = new HashMap<>();
+        pairs.put(')', '(');
+        pairs.put(']', '[');
+        pairs.put('}', '{');
+
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            //模拟栈的弹栈和入栈
+            char ch = s.charAt(i);
+            if (pairs.containsKey(ch)) {
+                if (stack.peek() != pairs.get(ch)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
