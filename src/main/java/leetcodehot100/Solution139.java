f4833bc908bed68638b194dd85508da8bc7ff2ca
胥天昊 <1499487526@qq.com>
2024-04-17 16:18:48 +0800

    139

--- src/main/java/leetcodehot100/Solution139.java
+++ src/main/java/leetcodehot100/Solution139.java
@@ -1,25 +1,34 @@
 package leetcodehot100;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 这道题可以转换成，求装满背包s的前几位字符的方式有几种
+ * 这道题可以转换成背包问题：求装满背包s的字符的方式有几种
  */
 class Solution139 {
-    public boolean wordBreak(String s, String[] wordDict) {
-        //装满背包s的前几位字符的方式有几种
-        int[] dp = new int[s.length() + 1];
-        //初始化dp数组
-        dp[0] = 1;
-        for (int i = 0; i <= s.length(); i++) {
-            for (int j = 0; j < wordDict.length; j++) {
-                if (i >= wordDict[j].length()
-                        &&
-                        wordDict[j].equals(s.substring(i - wordDict[j].length(), i))) {
-                    dp[i] += dp[i - wordDict[j].length()];
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        // dp[i]表示字符串s的前i个字符是否可以被拆分成字典中的单词
+        boolean[] dp = new boolean[n + 1];
+        // 空字符串可以被拆分
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (String word : wordDict) {
+                //计算出来单词的长度
+                int len = word.length();
+                //如果串s的下标位置比当前单词长度大，并且字符串s的前i - len个字符可以被拆分成字典中的单词 并且现在len的长度也刚好匹配，就可以重置成true
+                if (i >= len && dp[i - len] && s.substring(i - len, i).equals(word)) {
+                    // 如果前面的子字符串可以被拆分，并且当前位置到前一个单词的长度的子字符串正好等于一个字典中的单词，
+                    // 就说明当前位置的前面子串全部可以被拆分
+                    dp[i] = true;
+                    break;
                 }
             }
         }
-        return dp[s.length()] > 0;
+
+        // 返回最终结果，即dp[n]，表示整个字符串s是否可以被拆分成字典中的单词
+        return dp[n];
     }
 }
