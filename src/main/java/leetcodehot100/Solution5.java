4712d2c58394b0199ec699fbdf8ea7f8d40fad19
胥天昊 <1499487526@qq.com>
2024-04-20 11:56:14 +0800

    其实也是暴力解法，但是对是否是回文字符串进行了优化代码

--- src/main/java/leetcodehot100/Solution5.java
+++ src/main/java/leetcodehot100/Solution5.java
@@ -1,18 +1,20 @@
 package leetcodehot100;
 
+/**
+ * 其实也是暴力解法，但是对是否是回文字符串进行了优化代码
+ */
 class Solution5 {
-
     public String longestPalindrome(String s) {
-        String longestPalindrome=s.substring(0,1);
+        String longestPalindrome = s.substring(0, 1);
         for (int len = 2; len <= s.length(); len++) {
             for (int start = 0; start < s.length() - len + 1; start++) {
-                int end = start + len ;
+                int end = start + len;
                 String string = s.substring(start, end);
-                if (isPalindrome(string)){
+                if (isPalindrome(string)) {
                     //如果是回文字符串
                     //记录最长的值
-                    if(string.length()>longestPalindrome.length()){
-                        longestPalindrome=string;
+                    if (string.length() > longestPalindrome.length()) {
+                        longestPalindrome = string;
                     }
                 }
             }
@@ -21,15 +23,17 @@ class Solution5 {
     }
 
     public boolean isPalindrome(String str) {
-        // 删除字符串中的所有非字母数字字符，并将字符串转换为小写
-        str = str.replaceAll("[^\\w]", "").toLowerCase();
-        // 使用StringBuilder来反转字符串
-        StringBuilder reversedStr = new StringBuilder(str).reverse();
-
-        // 比较原字符串和反转后的字符串是否相等
-        return str.equals(reversedStr.toString());
+        int start = 0;
+        int end = str.length() - 1;
+        // 从两端开始比较字符，直到找到不匹配的字符或者比较完整个字符串
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
     }
 
-
 }
-
