3d2b4a9c486a373e6c43a1163f856230095f20ad
胥天昊 <1499487526@qq.com>
2024-04-23 16:01:57 +0800

    287

---
+++ src/main/java/leetcodehot100/Solution287.java
@@ -0,0 +1,11 @@
+package leetcodehot100;
+
+//思路：
+//		数组中的每个元素num指向下一个元素nums[num]
+//		如果有重复数字，肯定存在一个环形链表
+//		环入口节点的前一个节点即为重复的元素
+class Solution287 {
+    public int findDuplicate(int[] nums) {
+
+    }
+}
