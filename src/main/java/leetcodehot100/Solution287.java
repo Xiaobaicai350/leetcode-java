822f11fdb9724ddb9e0efa39ccceb8cbd91f7df6
胥天昊 <1499487526@qq.com>
2024-04-23 16:05:49 +0800

    287

--- src/main/java/leetcodehot100/Solution287.java
+++ src/main/java/leetcodehot100/Solution287.java
@@ -6,6 +6,28 @@ package leetcodehot100;
 //		环入口节点的前一个节点即为重复的元素
 class Solution287 {
     public int findDuplicate(int[] nums) {
-
+        // 快指针，每次移动两步
+        int fast = nums[0];
+        // 慢指针，每次移动一步
+        int slow = nums[0];
+        // 快指针移动两步
+        fast = nums[nums[fast]];
+        // 慢指针移动一步
+        slow = nums[slow];
+        // 当快慢指针没有相遇时，继续移动
+        while (fast != slow) {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        }
+        // 重置快指针到起点
+        fast = nums[0];
+        // 快慢指针都每次移动一步，直到它们相遇
+        // 相遇点即为环的入口，也就是重复的数字
+        while (fast != slow) {
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        // 返回重复的数字
+        return slow;
     }
 }
