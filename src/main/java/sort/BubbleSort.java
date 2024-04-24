f3a1aef1d7a0516362f5c4b2ec70f3419aba7744
胥天昊 <1499487526@qq.com>
2024-04-24 09:02:38 +0800

    BubbleSort

--- src/main/java/sort/BubbleSort.java
+++ src/main/java/sort/BubbleSort.java
@@ -3,11 +3,17 @@ package sort;
 import sort.test.IArraySort;
 
 public class BubbleSort implements IArraySort {
-
+    /**
+     * 基础版本的冒泡排序
+     *
+     * @param arr
+     * @return
+     */
     @Override
     public int[] sort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             for (int j = 0; j < arr.length - i; j++) {
+                //注意这里比较的只有j
                 if (arr[j] > arr[j + 1]) {
                     int tmp = arr[j];
                     arr[j] = arr[j + 1];
