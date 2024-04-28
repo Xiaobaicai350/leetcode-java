faac3dca2a9e8ccdb358bcb10f3898ab12f2842b
胥天昊 <1499487526@qq.com>
2024-04-28 20:02:59 +0800

    206

--- src/main/java/leetcodehot100/Solution206.java
+++ src/main/java/leetcodehot100/Solution206.java
@@ -4,11 +4,14 @@ import haohao.ListNode;
 
 /**
  * 经典的翻转链表题
+ * 比如遍历链表从左往右
+ * pre为cur左边一个节点
+ * temp用于交换地址
  */
 class Solution206 {
     public ListNode reverseList(ListNode head) {
         ListNode pre = null;
-        ListNode cur = head;//设置cur为快指针
+        ListNode cur = head;
         ListNode temp = null;
         while (cur != null) {
             temp = cur.next;
