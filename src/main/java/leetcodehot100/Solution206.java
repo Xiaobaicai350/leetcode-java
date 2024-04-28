23fad67b472d4ec743c9406ce7788aed23087f76
胥天昊 <1499487526@qq.com>
2024-04-28 20:00:26 +0800

    206

--- src/main/java/leetcodehot100/Solution206.java
+++ src/main/java/leetcodehot100/Solution206.java
@@ -2,15 +2,19 @@ package leetcodehot100;
 
 import haohao.ListNode;
 
+/**
+ * 经典的翻转链表题
+ */
 class Solution206 {
     public ListNode reverseList(ListNode head) {
         ListNode pre = null;
         ListNode cur = head;//设置cur为快指针
         ListNode temp = null;
         while (cur != null) {
-            temp = cur.next;//保存原来的cur.next
-            cur.next = pre;//翻转链表
-            pre = cur;//更新cur和pre
+            temp = cur.next;
+            cur.next = pre;
+            //pre和cur都往后走
+            pre = cur;
             cur = temp;
         }
         return pre;
