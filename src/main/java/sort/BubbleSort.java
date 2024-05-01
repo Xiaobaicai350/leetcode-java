13da75bff700c7143a3f1962be4cb2f99b263b8a
胥天昊 <1499487526@qq.com>
2024-05-01 09:40:45 +0800

    冒泡排序

--- src/main/java/sort/BubbleSort.java
+++ src/main/java/sort/BubbleSort.java
@@ -8,7 +8,9 @@ import sort.test.IArraySort;
 public class BubbleSort implements IArraySort {
     @Override
     public void sort(int[] arr) {
+        //从第二个数字开始遍历，也就是说需要遍历len-1趟
         for (int i = 1; i < arr.length; i++) {
+            //每一趟需要比较多少次。比如说十个数：第1趟需要遍历9次，第2趟要遍历8次。
             for (int j = 0; j < arr.length - i; j++) {
                 //注意这里比较的只有j
                 if (arr[j] > arr[j + 1]) {
