038a8abcb5c64e0ce03f77f21e9cc9963832a75e
胥天昊 <1499487526@qq.com>
2024-05-02 22:25:29 +0800

    53

--- src/main/java/leetcodehot100/Solution53.java
+++ src/main/java/leetcodehot100/Solution53.java
@@ -1,10 +1,16 @@
 package leetcodehot100;
 
+/*
+输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+输出：6
+解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ */
 class Solution53 {
     public int maxSubArray(int[] nums) {
         // dp[n]表示以第n个元素为结尾的最大子数组的和
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
+        //用max来记录数组最大子数组的和
         int max = dp[0];
         for (int i = 1; i < nums.length; i++) {
             if (dp[i - 1] >= 0) {//如果大于等于0再加
