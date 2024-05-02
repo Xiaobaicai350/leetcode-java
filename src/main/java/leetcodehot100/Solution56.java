03d3e39f3e376866395b060f4a7764996e28a778
胥天昊 <1499487526@qq.com>
2024-05-02 22:45:45 +0800

    56

--- src/main/java/leetcodehot100/Solution56.java
+++ src/main/java/leetcodehot100/Solution56.java
@@ -5,6 +5,15 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
+/*
+输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+输出：[[1,6],[8,10],[15,18]]
+解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ */
+
+/**
+ * 核心思想是用下一个的左边界判断目前的最大右边界，判断是否重叠
+ */
 class Solution56 {
     public int[][] merge(int[][] intervals) {
         // 新建结果集
