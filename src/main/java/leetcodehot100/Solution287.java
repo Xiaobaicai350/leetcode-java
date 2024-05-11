1b6191f1dca88c70a099bb9f5589535e71671d92
胥天昊 <1499487526@qq.com>
2024-05-11 12:41:04 +0800

    287

--- src/main/java/leetcodehot100/Solution287.java
+++ src/main/java/leetcodehot100/Solution287.java
@@ -4,30 +4,23 @@ package leetcodehot100;
 //		数组中的每个元素num指向下一个元素nums[num]
 //		如果有重复数字，肯定存在一个环形链表
 //		环入口节点的前一个节点即为重复的元素
+/*
+输入：nums = [1,3,4,2,2]
+输出：2
+ */
 class Solution287 {
     public int findDuplicate(int[] nums) {
-        // 快指针，每次移动两步
-        int fast = nums[0];
-        // 慢指针，每次移动一步
-        int slow = nums[0];
-        // 快指针移动两步
-        fast = nums[nums[fast]];
-        // 慢指针移动一步
-        slow = nums[slow];
-        // 当快慢指针没有相遇时，继续移动
-        while (fast != slow) {
+        int slow = 0, fast = 0;
+        //slow走一步，fast走两步，直到slow==fast，这时就可以让slow置零了
+        do {
+            slow = nums[slow];
             fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
             slow = nums[slow];
-        }
-        // 重置快指针到起点
-        fast = nums[0];
-        // 快慢指针都每次移动一步，直到它们相遇
-        // 相遇点即为环的入口，也就是重复的数字
-        while (fast != slow) {
             fast = nums[fast];
-            slow = nums[slow];
         }
-        // 返回重复的数字
         return slow;
     }
 }
