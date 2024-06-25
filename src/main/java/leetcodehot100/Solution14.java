15ccd8afd61614b1db3c7e6da5cef07e939a29c0
Xiaobaicai350 <1499487526@qq.com>
2024-06-25 19:22:13 +0800

    14

---
+++ src/main/java/leetcodehot100/Solution14.java
@@ -0,0 +1,30 @@
+package leetcodehot100;
+
+/*
+输入：strs = ["flower","flow","flight"]
+输出："fl"
+ */
+class Solution14 {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        //保存第一个字符串的长度
+        int length = strs[0].length();
+        //保存一共有多少个字符串
+        int count = strs.length;
+        //循环第一个字符串
+        for (int i = 0; i < length; i++) {
+            char c = strs[0].charAt(i);
+            //从第二个字符串开始循环
+            for (int j = 1; j < count; j++) {
+                //当循环到某个字符串的最后一位了||不等于的时候，就可以进行退出了
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        //到这说明是所有字符串都跟strs[0]相等，直接返回就行
+        return strs[0];
+    }
+}
