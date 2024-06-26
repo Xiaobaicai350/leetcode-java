85fb2992e9009c2517427535ddb18c8eb14ee536
Xiaobaicai350 <1499487526@qq.com>
2024-06-26 21:18:00 +0800

    腾讯音乐笔试

---
+++ src/main/java/腾讯音乐笔试0905/Demo1.java
@@ -0,0 +1,102 @@
+package 腾讯音乐笔试0905;
+
+import java.util.ArrayList;
+
+public class Demo1 {
+    class Solution {
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         *
+         * @param a int整型ArrayList
+         * @return int整型ArrayList
+         */
+        public ArrayList<Integer> newArray (ArrayList<Integer> a) {
+            // write code here
+            for(int i=0;i<a.size();i++){
+                String string = a.get(i).toString();
+                int res=delete2(string);
+                if(res==-1){
+                    a.remove(i);
+                }else {
+                    a.set(i,res);
+                }
+            }
+            return a;
+        }
+        //如果是22这种，就返回-1，其他的就进行拼接
+        //121
+        //12
+        public int delete2(String string){
+            String[] strings = string.split("2");
+            StringBuilder builder=new StringBuilder();
+            for (int i = 0; i < strings.length; i++) {
+                builder.append(strings[i]);
+            }
+            String res = builder.toString();
+            if(res.equals("")){
+                return -1;
+            }else {
+                return string2int(res);
+            }
+        }
+        public int string2int(String string){
+            int res=0;
+            char[] chars = string.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                res=res*10+(chars[i]-'0');
+            }
+            return res;
+        }
+    }
+
+
+
+
+    class Solution2 {
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         *
+         * @param a int整型ArrayList
+         * @return int整型ArrayList
+         */
+        public ArrayList<Integer> newArray (ArrayList<Integer> a) {
+            // write code here
+            for(int i=0;i<a.size();i++){
+                String string = a.get(i).toString();
+                String newStr = string.replace("2", "");
+                if(newStr.equals("")){
+                    a.remove(i);
+                }else {
+                    a.set(i,string2int(newStr));
+                }
+            }
+            return a;
+        }
+        //如果是22这种，就返回-1，其他的就进行拼接
+        //121
+        //12
+        public int delete2(String string){
+            String[] strings = string.split("2");
+            StringBuilder builder=new StringBuilder();
+            for (int i = 0; i < strings.length; i++) {
+                builder.append(strings[i]);
+            }
+            String res = builder.toString();
+            if(res.equals("")){
+                return -1;
+            }else {
+                return string2int(res);
+            }
+        }
+        public int string2int(String string){
+            int res=0;
+            char[] chars = string.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                res=res*10+(chars[i]-'0');
+            }
+            return res;
+        }
+    }
+}
---
+++ src/main/java/腾讯音乐笔试0905/Demo2.java
@@ -0,0 +1,67 @@
+package 腾讯音乐笔试0905;
+
+import haohao.ListNode;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Demo2 {
+    public class Solution {
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         *
+         * @param a ListNode类
+         * @param b ListNode类
+         * @return ListNode类
+         */
+        public ListNode mergeList (ListNode a, ListNode b) {
+            ArrayList<Integer> ints1=getV(a);
+            ArrayList<Integer> ints2=getV(b);
+            int end=0;
+            for (int i = 0; i < ints1.size()&&i<ints2.size(); i++) {
+                if(!Objects.equals(ints1.get(i), ints2.get(i))){
+                    end=i;
+                    break;
+                }
+            }
+            int start=0;
+            for (int i = ints1.size()-1, j=ints2.size()-1; i>0&&j>0 ; i--,j--) {
+                if(!Objects.equals(ints1.get(i), ints2.get(j))){
+                    start=i;
+                    break;
+                }
+            }
+            ListNode head=combineListNode(ints1,end,start);
+            return head.next;
+        }
+
+        private ListNode combineListNode(ArrayList<Integer> ints1, int end, int start) {
+            ListNode head = new ListNode(0);
+            ListNode cur=head;
+            System.out.println("end:"+end);
+            System.out.println("start:"+start);
+
+            for (int i = 0; i < ints1.size(); i++) {
+                if(i > end-1 && i <=start){
+                    continue;
+                }else {
+                    ListNode newNode = new ListNode(ints1.get(i));
+                    cur.next=newNode;
+                    cur=cur.next;
+                }
+            }
+            return head;
+        }
+
+        private ArrayList<Integer> getV(ListNode a) {
+            ListNode cur=a;
+            ArrayList<Integer> list = new ArrayList<>();
+            while(cur!=null){
+                list.add(cur.val);
+                cur=cur.next;
+            }
+            return list;
+        }
+    }
+}
