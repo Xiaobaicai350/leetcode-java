d43e46b70a5e6399a44b692f6f0d029870f6b7b6
xutianhao <1499487526@qq.com>
2024-09-06 10:27:47 +0800

    腾讯音乐笔试

--- src/main/java/腾讯音乐笔试0905/Demo1.java
+++ src/main/java/腾讯音乐笔试0905/Demo1.java
@@ -62,33 +62,17 @@ public class Demo1 {
          * @return int整型ArrayList
          */
         public ArrayList<Integer> newArray (ArrayList<Integer> a) {
+            ArrayList<Integer> res = new ArrayList<>();
             // write code here
             for(int i=0;i<a.size();i++){
                 String string = a.get(i).toString();
                 String newStr = string.replace("2", "");
                 if(newStr.equals("")){
-                    a.remove(i);
                 }else {
-                    a.set(i,string2int(newStr));
+                    res.add(string2int(newStr));
                 }
             }
-            return a;
-        }
-        //如果是22这种，就返回-1，其他的就进行拼接
-        //121
-        //12
-        public int delete2(String string){
-            String[] strings = string.split("2");
-            StringBuilder builder=new StringBuilder();
-            for (int i = 0; i < strings.length; i++) {
-                builder.append(strings[i]);
-            }
-            String res = builder.toString();
-            if(res.equals("")){
-                return -1;
-            }else {
-                return string2int(res);
-            }
+            return res;
         }
         public int string2int(String string){
             int res=0;
