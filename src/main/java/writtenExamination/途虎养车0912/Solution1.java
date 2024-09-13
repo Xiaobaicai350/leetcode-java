2d95e3a890346d06cf23acf7ffb3556ba7324e5f
xutianhao <1499487526@qq.com>
2024-09-13 10:36:43 +0800

    途虎养车笔试1

--- src/main/java/writtenExamination/途虎养车0912/Solution1.java
+++ src/main/java/writtenExamination/途虎养车0912/Solution1.java
@@ -9,7 +9,7 @@ public class Solution1 {
         ints[0] = new int[]{1, 3, 1};
         ints[1] = new int[]{-1, -1, 1};
         ints[2] = new int[]{4, -1, 1};
-        new Solution().maxPriceValue(ints);
+        System.out.println(new Solution().maxPriceValue(ints));
     }
 }
 
@@ -21,28 +21,49 @@ class Solution {
      * @return int整型
      */
     public int maxPriceValue(int[][] grid) {
-        int[][] dp = new int[grid.length][grid[0].length];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                dp[i][j] = -10000;
-            }
-        }
-        dp[0][0] = grid[0][0];
-        for (int i = 1; i < grid.length; i++) {
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
-        }
-        for (int i = 1; i < grid[0].length; i++) {
-            dp[0][i] = dp[0][i - 1] + grid[0][i];
-        }
-        for (int i = 1; i < grid.length; i++) {
-            for (int j = 1; j < grid[0].length; j++) {
-                if(grid[i][j]!=-1)
-                dp[i][j] = Math.max(dp[i - 1][j] + grid[i][j], dp[i][j - 1] + grid[i][j]);
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+
+        // 初始化第一行
+        for (int j = 0; j < n; j++) {
+            if (j == 0) {
+                dp[0][j] = grid[0][j];
+            } else if (grid[0][j] != -1 && grid[0][j - 1] != -1) {
+                dp[0][j] = dp[0][j - 1] + grid[0][j];
+            } else {
+                dp[0][j] = Integer.MIN_VALUE;
             }
         }
 
-        return dp[grid.length - 1][grid[0].length - 1];
+        // 初始化第一列
+        for (int i = 0; i < m; i++) {
+            if (i == 0) {
+                dp[i][0] = grid[i][0];
+            } else if (grid[i][0] != -1 && grid[i - 1][0] != -1) {
+                dp[i][0] = dp[i - 1][0] + grid[i][0];
+            } else {
+                dp[i][0] = Integer.MIN_VALUE;
+            }
+        }
+
+        // 动态规划填表
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (grid[i][j] == -1) {
+                    dp[i][j] = Integer.MIN_VALUE;
+                } else {
+                    int fromTop = grid[i][j] + (dp[i - 1][j] == Integer.MIN_VALUE ? 0 : dp[i - 1][j]);
+                    int fromLeft = grid[i][j] + (dp[i][j - 1] == Integer.MIN_VALUE ? 0 : dp[i][j - 1]);
+                    dp[i][j] = Math.max(fromTop, fromLeft);
+                }
+            }
+        }
+
+        // 检查最后一个元素是否有效
+        return dp[m - 1][n - 1] == Integer.MIN_VALUE ? 0 : dp[m - 1][n - 1];
     }
+
 
 
 }
