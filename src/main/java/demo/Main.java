a88cd289bd695b6e88394928243ab890db126b54
xutianhao <1499487526@qq.com>
2024-09-20 11:46:54 +0800

    Add java-diff-utils dependency and implement file comparison in Main.java

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,16 +1,54 @@
 package demo;
-// 本题为考试多行输入输出规范示例，无需提交，不计分。
 
-import java.util.HashMap;
-import java.util.Scanner;
+import com.github.difflib.DiffUtils;
+import com.github.difflib.patch.AbstractDelta;
+import com.github.difflib.patch.Chunk;
+import com.github.difflib.patch.Patch;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            HashMap<Integer, Integer> map = new HashMap<>();
 
-        }
+    public static void main(String[] args) {
+        List<String> original = Arrays.asList("This is a test senctence." +
+                "\r\nThis is a test for diffutils");
+        //对比文件
+        List<String> revised = Arrays.asList("This is a test senctence." +
+                "\r\nThis is a test for diffutils1");
+
+
+        //两文件的不同点
+        Patch<String> patch = DiffUtils.diff(original, revised);
+        System.out.println(patch);
+        List<AbstractDelta<String>> deltas = patch.getDeltas();
+        deltas.forEach(delta -> {
+            System.out.println(delta.getType());
+            switch (delta.getType()) {
+                case INSERT:
+                    //新增
+                    System.out.println("新增");
+                    Chunk<String> insert = delta.getTarget();
+                    int position = insert.getPosition();
+                    System.out.println("+ " + (position + 1) + " " + insert.getLines());
+                    break;
+                case CHANGE:
+                    //修改
+                    System.out.println("修改");
+                    Chunk<String> source = delta.getSource();
+                    Chunk<String> target1 = delta.getTarget();
+                    System.out.println("\n- " + (source.getPosition() + 1) + " " + source.getLines() + "\n+ " + "" + (target1.getPosition() + 1) + " " + target1.getLines());
+                    break;
+                case DELETE:
+                    //删除
+                    System.out.println("删除");
+                    Chunk<String> delete = delta.getSource();
+                    System.out.println("- " + (delete.getPosition() + 1) + " " + delete.getLines());
+                    break;
+                case EQUAL:
+                    System.out.println("无变化");
+                    break;
+            }
+        });
     }
 }
