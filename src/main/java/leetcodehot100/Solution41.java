a12a353f51134d58c10e33d3bc975691caa9df2d
xutianhao <1499487526@qq.com>
2024-10-09 11:02:38 +0800

    41

--- src/main/java/leetcodehot100/Solution41.java
+++ src/main/java/leetcodehot100/Solution41.java
@@ -1,9 +1,17 @@
 package leetcodehot100;
 
+/*
+给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案
+输入：nums = [3,4,-1,1]
+输出：2
+解释：1 在数组中，但 2 没有。
+ */
 class Solution41 {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
         for (int i = 0; i < n; i++) {
+            // 交换nums[i]和nums[nums[i]-1]的位置
             while (nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
                 swap(nums, i, nums[i] - 1);
             }
