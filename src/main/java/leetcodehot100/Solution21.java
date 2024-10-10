7e9c11606c06bedbafe6b6e7c90d10b56424c419
xutianhao <1499487526@qq.com>
2024-10-10 11:43:31 +0800

    21

--- src/main/java/leetcodehot100/Solution21.java
+++ src/main/java/leetcodehot100/Solution21.java
@@ -2,7 +2,7 @@ package leetcodehot100;
 
 import entity.ListNode;
 
-
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
 class Solution21 {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         // 新建链表的新头节点
