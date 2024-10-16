bad18b896b73da69c58c3dd94fc4a0208d409a8c
xutianhao <1499487526@qq.com>
2024-10-16 11:07:20 +0800

    baidu笔试

--- src/main/java/writtenExamination/baidu笔试1015/Main1.java
+++ src/main/java/writtenExamination/baidu笔试1015/Main1.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  */
 //12345 2   1 3    2
 public class Main1 {
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         Scanner in = new Scanner(System.in);
         int count = in.nextInt();
         while (count-- != 0) {
@@ -23,6 +23,22 @@ public class Main1 {
                 System.out.println(k);
             } else {
                 System.out.println(1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt(); // 测试用例的数量
+        while (t-- > 0) {
+            long n = scanner.nextLong(); // 数组的长度
+            long k = scanner.nextLong(); // 指定的位置
+            // 如果k小于等于(n+1)/2，则直接输出k
+            // 否则，输出n+1-k
+            if (k <= (n + 1) / 2) {
+                System.out.println(k);
+            } else {
+                System.out.println(n + 1 - k);
             }
         }
     }
