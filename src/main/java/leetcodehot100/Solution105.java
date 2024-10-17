574415f4514c856616e9909b6507f47718fa3e55
xutianhao <1499487526@qq.com>
2024-10-17 15:50:50 +0800

    105

--- src/main/java/leetcodehot100/Solution105.java
+++ src/main/java/leetcodehot100/Solution105.java
@@ -11,40 +11,41 @@ import java.util.Map;
  */
 class Solution105 {
     // 构造哈希映射，帮助我们快速定位根节点
-    //key存储的是中序遍历的值，value存储的是对应的下标
-    Map<Integer, Integer> indexMap = new HashMap<Integer, Integer>();
+    // key存储的是中序遍历的值，value存储的是对应的下标
+    private Map<Integer, Integer> indexMap;
+    private int[] preorder;
+    private int preorderIndex;
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         int n = preorder.length;
+        this.preorder = preorder;
+        this.preorderIndex = 0;
+        this.indexMap = new HashMap<>();
         for (int i = 0; i < n; i++) {
             indexMap.put(inorder[i], i);
         }
-        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+        return buildTree(0, n - 1);
     }
 
-    public TreeNode myBuildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
-        //递归终止条件
-        if (preorder_left > preorder_right || inorder_left > preorder_right) {
+    private TreeNode buildTree(int inorderLeft, int inorderRight) {
+        if (inorderLeft > inorderRight) {
             return null;
         }
 
-        // 前序遍历中的第一个节点就是根节点，注意得到的是下标
-        int preorder_root = preorder_left;
-        // 在中序遍历中定位根节点，注意这里得到的是下标
-        int inorder_root = indexMap.get(preorder[preorder_root]);
+        // 前序遍历中的第一个节点就是根节点
+        int rootVal = preorder[preorderIndex++];
+        // 在中序遍历中定位根节点
+        int inorderRoot = indexMap.get(rootVal);
 
         // 先把根节点建立出来
-        TreeNode root = new TreeNode(preorder[preorder_root]);
-        // 得到左子树中的节点数目
-        int size_left_subtree = inorder_root - inorder_left;
+        TreeNode root = new TreeNode(rootVal);
         // 递归地构造左子树，并连接到根节点
-        // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
-        root.left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
-
+        root.left = buildTree(inorderLeft, inorderRoot - 1);
         // 递归地构造右子树，并连接到根节点
-        // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
-        root.right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
+        root.right = buildTree(inorderRoot + 1, inorderRight);
         return root;
     }
 }
+
+
 
