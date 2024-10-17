26de42e06e127c1269b64a93b7b1e9e430dc219d
xutianhao <1499487526@qq.com>
2024-10-17 16:02:23 +0800

    437

--- src/main/java/leetcodehot100/Solution437.java
+++ src/main/java/leetcodehot100/Solution437.java
@@ -2,6 +2,13 @@ package leetcodehot100;
 
 import entity.TreeNode;
 
+/*
+输入：root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+输出：3
+解释：和等于 8 的路径有 3 条，如图所示。
+给定一个二叉树的根节点 root ，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
+路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+ */
 class Solution437 {
     public int pathSum(TreeNode root, int targetSum) {
         //安全性校验，如果root为空，说明这棵树都不存在。
