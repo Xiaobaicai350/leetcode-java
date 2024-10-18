885d0d9eb92ce7e3ae98281eb4d41bbff37dae46
xutianhao <1499487526@qq.com>
2024-10-18 10:24:44 +0800

    46

--- src/main/java/leetcodehot100/Solution46.java
+++ src/main/java/leetcodehot100/Solution46.java
@@ -3,6 +3,13 @@ package leetcodehot100;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ */
+/*
+输入：nums = [1,2,3]
+输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ */
 class Solution46 {
     // 最终的结果集
     private List<List<Integer>> ans = new ArrayList<>();
