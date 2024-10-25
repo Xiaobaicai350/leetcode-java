df2625d23f9945e42441c5c918fe964742bfb3c7
xutianhao <1499487526@qq.com>
2024-10-25 14:57:22 +0800

    txyz

--- src/main/java/writtenExamination/腾讯云智笔试1022/txyz.sql
+++ src/main/java/writtenExamination/腾讯云智笔试1022/txyz.sql
@@ -1,1 +1,8 @@
 select user_id from cv_test where user_id in (select user_id from user_deliver group by user_id having sum(deliver_num)>10) and score>75;
+--同理
+SELECT cv.user_id
+FROM cv_test cv, user_deliver ud
+WHERE cv.user_id = ud.user_id
+AND cv.score > 75
+GROUP BY cv.user_id
+HAVING SUM(ud.deliver_num) > 10;
