e798ed731b3e6323ddb05e90a44a40242485d2df
xutianhao <1499487526@qq.com>
2024-10-29 15:30:04 +0800

    104

--- src/main/java/leetcodehot100/Solution104.java
+++ src/main/java/leetcodehot100/Solution104.java
@@ -2,10 +2,14 @@ package leetcodehot100;
 
 import entity.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 求最大的深度
  */
 class Solution104 {
+    //递归方法
     public int maxDepth(TreeNode root) {
         if (root == null) {//递归出口
             return 0;
@@ -15,5 +19,32 @@ class Solution104 {
             //然后加上根节点这一层的高度就是1了
             return max + 1;
         }
+    }
+    //迭代方法求二叉树的最大深度
+    public int maxDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size(); // 当前层的节点数
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                // 将当前节点的子节点加入队列
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+            depth++; // 完成一层的遍历，深度加1
+        }
+
+        return depth;
     }
 }
