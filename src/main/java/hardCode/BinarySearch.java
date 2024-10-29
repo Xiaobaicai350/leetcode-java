d79a729da238330114ddefe47a7715cae12f05cd
xutianhao <1499487526@qq.com>
2024-10-29 15:34:52 +0800

    二分

--- src/main/java/hardCode/BinarySearch.java
+++ src/main/java/hardCode/BinarySearch.java
@@ -1,11 +1,12 @@
 package hardCode;
 
 public class BinarySearch {
-    public static int commonBinarySearch(int[] arr,int key) {
+    public static int commonBinarySearch(int[] arr, int key) {
         int low = 0;
         int high = arr.length - 1;
         int middle = 0;            //定义middle
         while (low <= high) {
+            //middle = low + (high - low) / 2;//跟下方等价 防止溢出
             middle = (low + high) / 2;
             if (arr[middle] > key) {
                 //比关键字大则关键字在左区域
@@ -17,7 +18,6 @@ public class BinarySearch {
                 return middle;
             }
         }
-
         return -1;        //最后仍然没有找到，则返回-1
     }
 }
