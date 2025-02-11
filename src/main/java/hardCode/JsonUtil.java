5996e1da25b378d40c22a82f02f1580f6021f6a5
天昊 <1499487526@qq.com>
2025-02-11 15:05:00 +0800

    更改gson

--- src/main/java/hardCode/JsonUtil.java
+++ src/main/java/hardCode/JsonUtil.java
@@ -6,15 +6,6 @@ import com.google.gson.Gson;
 public class JsonUtil {
     private static final Gson gson = new Gson();
 
-    //将JSON字符串转换为指定类型的对象。
-    public static <T> T fromJson(String json, Class<T> classOfT) {
-        return gson.fromJson(json, classOfT);
-    }
-
-    //将对象转换为JSON字符串。
-    public static String toJson(Object src) {
-        return gson.toJson(src);
-    }
 
     public static void main(String[] args) {
         // 示例对象
@@ -32,11 +23,11 @@ public class JsonUtil {
         String jsonString = "{\"name\":\"John\", \"age\":30}";
 
         // JSON转对象
-        Person person = fromJson(jsonString, Person.class);
+        Person person = gson.fromJson(jsonString, Person.class);
         System.out.println(person); // 输出: Person{name='John', age=30}
 
         // 对象转JSON
-        String json = toJson(person);
+        String json = gson.toJson(person);
         System.out.println(json); // 输出: {"name":"John","age":30}
     }
 }
