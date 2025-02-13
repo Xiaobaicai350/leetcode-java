ea8ba92e65652d17c52b4fd3b3bd147124d0d10f
天昊 <1499487526@qq.com>
2025-02-13 11:22:18 +0800

    17

--- src/main/java/leetcodehot100/Solution17.java
+++ src/main/java/leetcodehot100/Solution17.java
@@ -5,6 +5,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/*
+给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+示例 1：
+输入：digits = "23"
+输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+示例 2：
+输入：digits = ""
+输出：[]
+
+示例 3：
+输入：digits = "2"
+输出：["a","b","c"]
+ */
 class Solution17 {
     Map<Character, String> phoneMap = new HashMap<>();
 
@@ -19,8 +35,10 @@ class Solution17 {
         phoneMap.put('9', "wxyz");
     }
 
+    //结果集
     List<String> combinations = new ArrayList<>();
 
+    //入口，返回所有它能表示的字母组合
     public List<String> letterCombinations(String digits) {
         //特殊情况特殊处理
         if (digits.equals("")) {
@@ -30,6 +48,12 @@ class Solution17 {
         return combinations;
     }
 
+
+    /**
+     * @param digits      输入的数字
+     * @param index       数字的下标
+     * @param combination 组合
+     */
     private void backtrack(String digits, int index, String combination) {
         if (index == digits.length()) {
             combinations.add(combination);
