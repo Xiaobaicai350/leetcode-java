76746dc5e5b0bc7806bd3d1b6073192709090a54
天昊 <1499487526@qq.com>
2025-02-13 11:26:42 +0800

    20

--- src/main/java/leetcodehot100/Solution20.java
+++ src/main/java/leetcodehot100/Solution20.java
@@ -5,7 +5,32 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
+/*
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+有效字符串需满足：
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+每个右括号都有一个对应的相同类型的左括号。
+
+
+示例 1：
+输入：s = "()"
+输出：true
+
+示例 2：
+输入：s = "()[]{}"
+输出：true
+
+示例 3：
+输入：s = "(]"
+输出：false
+
+示例 4：
+输入：s = "([])"
+输出：true
+ */
 class Solution20 {
+    //判断字符串是否有效
     public boolean isValid(String s) {
         int n = s.length();
         //如果长度为奇数，直接返回
@@ -18,17 +43,18 @@ class Solution20 {
         pairs.put(']', '[');
         pairs.put('}', '{');
 
+        //新建一个栈
         Deque<Character> stack = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             //模拟栈的弹栈和入栈
-            char ch = s.charAt(i);//()
+            char ch = s.charAt(i);//
             if (pairs.containsKey(ch)) {//如果是)]}的话
                 if (stack.peek() != pairs.get(ch)) {//需要判断栈顶
                     return false;
                 }
                 stack.pop();
-            } else {
-                stack.push(ch);
+            } else {//如果是([{的话
+                stack.push(ch);//压栈
             }
         }
         return stack.isEmpty();
