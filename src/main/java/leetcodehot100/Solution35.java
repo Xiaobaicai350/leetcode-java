fc055622dbf563778037acdc7a1257111bed1fb9
天昊 <1499487526@qq.com>
2025-02-14 11:06:14 +0800

    35

--- src/main/java/leetcodehot100/Solution35.java
+++ src/main/java/leetcodehot100/Solution35.java
@@ -1,5 +1,21 @@
 package leetcodehot100;
 
+/*
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+请必须使用时间复杂度为 O(log n) 的算法。
+
+示例 1:
+输入: nums = [1,3,5,6], target = 5
+输出: 2
+
+示例 2:
+输入: nums = [1,3,5,6], target = 2
+输出: 1
+
+示例 3:
+输入: nums = [1,3,5,6], target = 7
+输出: 4
+ */
 class Solution35 {
     public int searchInsert(int[] nums, int target) {
         int n = nums.length;
@@ -8,12 +24,15 @@ class Solution35 {
         int ans = n;
         while (left <= right) {
             //计算中间位置
-            int mid = (right + left) >> 1;
-            if (target <= nums[mid]) {
+            int mid = (right + left) / 2;
+            if (target == nums[mid]) {//如果直接找到了，直接返回就可以了
+                ans = mid;
+                return ans;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {//如果target小于中间值，说明就是插入到左边界到中间之间的位置，我们用ans来记录
                 ans = mid;
                 right = mid - 1;
-            } else {
-                left = mid + 1;
             }
         }
         return ans;
