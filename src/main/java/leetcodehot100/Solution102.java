4b498c033b29feb509cfd974a9060fdf01f07925
天昊 <1499487526@qq.com>
2025-02-24 11:36:14 +0800

    102

--- src/main/java/leetcodehot100/Solution102.java
+++ src/main/java/leetcodehot100/Solution102.java
@@ -7,6 +7,21 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+/*
+给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+
+示例 1：
+输入：root = [3,9,20,null,null,15,7]
+输出：[[3],[9,20],[15,7]]
+
+示例 2：
+输入：root = [1]
+输出：[[1]]
+
+示例 3：
+输入：root = []
+输出：[]
+ */
 public class Solution102 {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
