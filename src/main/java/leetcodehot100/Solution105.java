ca2719f4a8f16b1f13e9e3dc7773469a8cbc88d8
天昊 <1499487526@qq.com>
2025-02-25 10:19:01 +0800

    105

--- src/main/java/leetcodehot100/Solution105.java
+++ src/main/java/leetcodehot100/Solution105.java
@@ -2,8 +2,26 @@ package leetcodehot100;
 
 import entity.TreeNode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+
+/*
+给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
+
+示例 1:
+输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+输出: [3,9,20,null,null,15,7]
+
+示例 2:
+输入: preorder = [-1], inorder = [-1]
+输出: [-1]
+
+思路：
+    前序遍历的第一个元素是根节点。
+    在中序遍历中找到根节点的位置，根节点左边的元素属于左子树，右边的元素属于右子树。
+    递归地构建左子树和右子树
+ */
 
 /**
  * 前序遍历 根左右
@@ -13,39 +31,43 @@ import java.util.Map;
 preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
  */
 class Solution105 {
-    // 构造哈希映射，帮助我们快速定位根节点
-    // key存储的是中序遍历的值，value存储的是对应的下标
-    private Map<Integer, Integer> indexMap;
-    private int[] preorder;
-    private int preorderIndex;
-
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int n = preorder.length;
-        this.preorder = preorder;
-        this.preorderIndex = 0;
-        this.indexMap = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            indexMap.put(inorder[i], i);
-        }
-        return buildTree(0, n - 1);
-    }
-
-    private TreeNode buildTree(int inorderLeft, int inorderRight) {
-        if (inorderLeft > inorderRight) {
+        if (preorder.length == 0 || inorder.length == 0) {
             return null;
         }
-
-        // 前序遍历中的第一个节点就是根节点
-        int rootVal = preorder[preorderIndex++];
-        // 在中序遍历中定位根节点
-        int inorderRoot = indexMap.get(rootVal);
-
-        // 先把根节点建立出来
+        // 前序遍历的第一个元素是根节点
+        int rootVal = preorder[0];
         TreeNode root = new TreeNode(rootVal);
-        // 递归地构造左子树，并连接到根节点
-        root.left = buildTree(inorderLeft, inorderRoot - 1);
-        // 递归地构造右子树，并连接到根节点
-        root.right = buildTree(inorderRoot + 1, inorderRight);
+
+        // 在中序遍历中找到根节点的位置
+        int rootIndexInorder = 0;
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == rootVal) {
+                rootIndexInorder = i;
+                break;
+            }
+        }
+        //输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+        /*
+        preorder = [9]
+        inorder = [9]
+         */
+        // 递归构建左子树
+        root.left = buildTree(
+                //Arrays.copyOfRange 方法是“左闭右开”的区间
+                Arrays.copyOfRange(preorder, 1, rootIndexInorder + 1),
+                Arrays.copyOfRange(inorder, 0, rootIndexInorder)
+        );
+        /*
+        preorder = [20, 15, 7]
+        inorder = [15, 20, 7]
+         */
+        // 递归构建右子树
+        root.right = buildTree(
+                Arrays.copyOfRange(preorder, rootIndexInorder + 1, preorder.length),
+                Arrays.copyOfRange(inorder, rootIndexInorder + 1, inorder.length)
+        );
+
         return root;
     }
 }
