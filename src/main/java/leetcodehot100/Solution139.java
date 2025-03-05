f3f2f66a9de3402ee0b81dda096e542c02925e72
天昊 <1499487526@qq.com>
2025-03-05 11:22:05 +0800

    139

--- src/main/java/leetcodehot100/Solution139.java
+++ src/main/java/leetcodehot100/Solution139.java
@@ -3,6 +3,25 @@ package leetcodehot100;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+给你一个字符串 s 和一个字符串列表 wordDict 作为字典。如果可以利用字典中出现的一个或多个单词拼接出 s 则返回 true。
+注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
+
+示例 1：
+输入: s = "leetcode", wordDict = ["leet", "code"]
+输出: true
+解释: 返回 true 因为 "leetcode" 可以由 "leet" 和 "code" 拼接成。
+
+示例 2：
+输入: s = "applepenapple", wordDict = ["apple", "pen"]
+输出: true
+解释: 返回 true 因为 "applepenapple" 可以由 "apple" "pen" "apple" 拼接成。
+     注意，你可以重复使用字典中的单词。
+
+示例 3：
+输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+输出: false
+ */
 /**
  * 这道题可以转换成背包问题：求装满背包s的字符的方式有几种
  */
@@ -18,7 +37,10 @@ class Solution139 {
             for (String word : wordDict) {
                 //计算出来单词的长度
                 int len = word.length();
-                //如果串s的下标位置比当前单词长度大，并且字符串s的前i - len个字符可以被拆分成字典中的单词 并且现在len的长度也刚好匹配，就可以重置成true
+                //如果
+                // s的下标位置比当前单词长度大（这个是后面判断条件的安全性校验），
+                // 并且字符串s的前i - len个字符可以被拆分成字典中的单词
+                // 并且现在len的长度也刚好匹配，就可以重置成true
                 if (i >= len && dp[i - len] && s.substring(i - len, i).equals(word)) {
                     // 如果前面的子字符串可以被拆分，并且当前位置到前一个单词的长度的子字符串正好等于一个字典中的单词，
                     // 就说明当前位置的前面子串全部可以被拆分
