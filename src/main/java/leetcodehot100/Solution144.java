075d2f61e8842c636b085e86103a847f0c844802
天昊 <1499487526@qq.com>
2025-03-05 11:31:02 +0800

    144

--- src/main/java/leetcodehot100/Solution144.java
+++ src/main/java/leetcodehot100/Solution144.java
@@ -6,6 +6,7 @@ import entity.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
 
+//给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
 class Solution144 {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
