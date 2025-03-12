ef2c0667cf4ba74235025d4fab223965d7a7dfa9
天昊 <1499487526@qq.com>
2025-03-12 11:25:07 +0800

    207

--- src/main/java/leetcodehot100/Solution207.java
+++ src/main/java/leetcodehot100/Solution207.java
@@ -22,45 +22,66 @@ import java.util.*;
 输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
 输出：false
 解释：总共有 2 门课程。学习课程 1 之前，你需要先完成课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。
+示例3:
+5, new int[][]{{0, 1}, {0, 2}, {1, 3}, {3, 4}, {1, 4}}
  */
 public class Solution207 {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // 存储有向图
-        List<List<Integer>> edges = new ArrayList<>();
-        // 存储每个节点的入度数量
-        int[] indeg = new int[numCourses];
-        // 初始化有向图
+        // 存储每个节点的出度
+        int[] outDegree = new int[numCourses];//要完成i这门课程前置条件有outDegree[i]门
+        // 存储反向图的邻接表
+        //reverseAdjList[0]中存储的是依赖0这门课程的课程
+        List<List<Integer>> reverseAdjList = new ArrayList<>();
         for (int i = 0; i < numCourses; i++) {
-            edges.add(new ArrayList<>());
+            reverseAdjList.add(new ArrayList<>());
         }
-        // 构建有向图并计算每个节点的入度
-        for (int[] info : prerequisites) {
-            edges.get(info[1]).add(info[0]);
-            indeg[info[0]]++;
+
+        // 计算每个节点的出度和构建反向邻接表
+        for (int[] prerequisite : prerequisites) {
+            int ai = prerequisite[0];
+            int bi = prerequisite[1];
+            // 这里 bi 依赖于 ai，所以 ai 的出度增加
+            outDegree[bi]++;
+            // 反向构建邻接表，从 ai 指向 bi
+            reverseAdjList.get(ai).add(bi);
         }
-        // 创建队列
-        Queue<Integer> q = new LinkedList<>();
-        // 第一次先把入度为0的节点入队
+
+        /*
+        outDegree: [0, 1, 1, 1, 2]
+        reverseAdjList: [[1, 2], [3, 4], [], [4], []]
+         */
+        System.out.println("outDegree: " + Arrays.toString(outDegree));
+        System.out.println("reverseAdjList: " + reverseAdjList);
+
+        // 存储出度为 0 的节点
+        Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
-            if (indeg[i] == 0) {
-                q.offer(i);
+            if (outDegree[i] == 0) {
+                queue.offer(i);
             }
         }
-        // 已经访问的节点数量
-        int visited = 0;
-        while (!q.isEmpty()) {
-            int u = q.poll();
-            visited++;
-            // 遍历当前节点的所有邻接节点
-            for (int v : edges.get(u)) {
-                indeg[v]--;
-                // 如果邻接节点的入度为0，则加入队列
-                if (indeg[v] == 0) {
-                    q.offer(v);
+
+        //queue: [0]
+        // 记录已经学习的课程数量
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            count++;
+            // 遍历当前课程的所有前置课程
+            for (int prevCourse : reverseAdjList.get(course)) {
+                // 前置课程的出度减 1
+                outDegree[prevCourse]--;
+                if (outDegree[prevCourse] == 0) {
+                    queue.offer(prevCourse);
                 }
             }
         }
-        // 如果访问的节点数量等于课程数量，说明可以完成所有课程
-        return visited == numCourses;
+
+        // 如果已经学习的课程数量等于总课程数量，则可以完成所有课程的学习
+        return count == numCourses;
+    }
+
+    public static void main(String[] args) {
+        new Solution207().canFinish(5, new int[][]{{0, 1}, {0, 2}, {1, 3}, {3, 4}, {1, 4}});
     }
 }
