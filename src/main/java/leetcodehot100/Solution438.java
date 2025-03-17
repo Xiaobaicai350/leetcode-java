153754e10092cf67ee08611588e2cf159234e6b8
天昊 <1499487526@qq.com>
2025-03-17 17:43:15 +0800

    438

--- src/main/java/leetcodehot100/Solution438.java
+++ src/main/java/leetcodehot100/Solution438.java
@@ -21,16 +21,28 @@ import java.util.List;
 //        return res;
 //    }
 
-/**
- * 这种方法的思想是:用两个数组来记录每一个字符出现了多少次，如果出现的次数相等，就说明是异构体
- */
 /*
+给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+
+示例 1:
 输入: s = "cbaebabacd", p = "abc"
 输出: [0,6]
 解释:
 起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
 起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
+
+ 示例 2:
+输入: s = "abab", p = "ab"
+输出: [0,1,2]
+解释:
+起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
+起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
+起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
  */
+/**
+ * 这种方法的思想是:用两个数组来记录每一个字符出现了多少次，如果出现的次数相等，就说明是异构体
+ */
+
 class Solution438 {
     public List<Integer> findAnagrams(String s, String p) {
         ArrayList<Integer> res = new ArrayList<>();
