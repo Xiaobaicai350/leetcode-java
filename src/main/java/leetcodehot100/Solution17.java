d8bcbd9415a278369bbeaf216a2b3d422bdf73bf
天昊 <1499487526@qq.com>
2025-03-21 15:57:29 +0800

    17

--- src/main/java/leetcodehot100/Solution17.java
+++ src/main/java/leetcodehot100/Solution17.java
@@ -1,9 +1,6 @@
 package leetcodehot100;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /*
 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
@@ -67,6 +64,37 @@ class Solution17 {
                 backtrack(digits, index + 1, combination + letters.charAt(i));
             }
         }
+    }
+
+
+    // 非递归方法
+    public List<String> letterCombinations1(String digits) {
+        List<String> combinations = new ArrayList<>();
+        // 特殊情况特殊处理
+        if (digits.isEmpty()) {
+            return combinations;
+        }
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer("");
+
+        for (int i = 0; i < digits.length(); i++) {//遍历输入的数字
+            char digit = digits.charAt(i);//得到当前位置上的数字
+            String letters = phoneMap.get(digit);//通过数字得到对应的字母
+            int size = queue.size();//得到队列的大小
+            for (int j = 0; j < size; j++) {//遍历队列
+                String current = queue.poll();//得到当前的字符串
+                for (char letter : letters.toCharArray()) {
+                    queue.offer(current + letter);//将当前的字符串加上字母，然后加入队列
+                }
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            combinations.add(queue.poll());
+        }
+
+        return combinations;
     }
 }
 
