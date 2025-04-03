68bc296b7cde113f8b59f24dc52792859406d654
天昊 <1499487526@qq.com>
2025-04-03 11:27:00 +0800

    21

--- src/main/java/leetcodehot100/Solution21.java
+++ src/main/java/leetcodehot100/Solution21.java
@@ -5,27 +5,24 @@ import entity.ListNode;
 //将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
 class Solution21 {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // 新建链表的新头节点
-        ListNode newHead = new ListNode();
-        ListNode cur = newHead;
-        ListNode a = list1;
-        ListNode b = list2;
-        while (a != null && b != null) {
-            if (a.val < b.val) {
-                cur.next = a;
-                a = a.next;
+        ListNode head = new ListNode();
+        ListNode cur = head;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                cur.next = list1;
+                cur = cur.next;
+                list1 = list1.next;
             } else {
-                cur.next = b;
-                b = b.next;
+                cur.next = list2;
+                cur = cur.next;
+                list2 = list2.next;
             }
-            cur = cur.next;
         }
-        // 到这里说明a遍历完了或者是b遍历完了
-        if (a == null) {
-            cur.next = b;
+        if (list1 == null) {
+            cur.next = list2;
         } else {
-            cur.next = a;
+            cur.next = list1;
         }
-        return newHead.next;
+        return head.next;
     }
 }
