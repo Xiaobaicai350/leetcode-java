2ca028d65979076ef987e9502657d2fe82388af5
天昊 <1499487526@qq.com>
2025-04-07 19:54:33 +0800

    24

--- src/main/java/leetcodehot100/Solution24.java
+++ src/main/java/leetcodehot100/Solution24.java
@@ -5,11 +5,13 @@ import entity.ListNode;
 /**
  * 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
  * 用手画一下逻辑就好了
+ * 1-》2-》3-》4
+ * 2-》1-》4-》3
  */
 class Solution24 {
     public ListNode swapPairs(ListNode head) {
         // 创建虚拟头结点
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode();
         // 给dummy赋初值
         dummy.next = head;
         ListNode cur = dummy;
@@ -19,7 +21,7 @@ class Solution24 {
             cur.next = cur.next.next;// 1
             cur.next.next = temp1;// 2
             cur.next.next.next = temp2;// 3
-            cur = cur.next.next;//// cur移动两位，准备下一轮交换
+            cur = cur.next.next;// cur移动两位，准备下一轮交换
         }
         return dummy.next;
     }
